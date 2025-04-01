98d6368aa4a916886d069461d3417ad98248651c
Mery Kassis <MeryKassis48@gmail.com>
2025-04-01 15:07:05 +0300

    Add country service

---
+++ Mottrist.Service/Features/Country/Interfaces/ICountryService.cs
@@ -0,0 +1,12 @@
+﻿
+using Mottrist.Service.Features.General;
+
+namespace Mottrist.Service.Features.Country.Interfaces
+{
+    public interface ICountryService : IBaseService
+    {
+
+        bool CountryExists(int Id);
+
+    }
+}
